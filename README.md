2c8e275db08afe434d3dcb8b6a447819cddfcfda
Kenyon Gordon <kenyongordon@MacBook-Pro-2.local>
2023-05-04 21:54:45 -0500

    commit all changes

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# groupproject
+# group-project-1
+
+## Description
+Our motivation was to build an application that would allow us to practice integrating more than one API in an application, styling with Bulma, and working with local storage. We built this project so that users could research Wiki articles and alleviate cognitive fatigue. We learned about the importance of security measures in repos. During our first phase we were headed in a  different direction and had exposed an API key in a commit. We received a notification from GitGuardian and took measures to fix that issue. We pivoted and now off this ad-free application that can be used to search any articles that can be found on Wikipedia as well as can be utilized to fetch random dog pictures so users can have a chance to relieve cognitive dulling.
+
+## Usage
+
+   ![wide](./assets/images/wide.png)
+
+   ![mobile](./assets/images/mobile-top.png)
+
+   ![pup](./assets/images/mobile-pup.png)
+### To use the application, click on this link:
+
+https://perfectblue0.github.io/group-project-1/
+
+### Repo Link
+
+https://github.com/perfectblue0/group-project-1
+
+Next, type in a topic of your choosing in the search bar and click the magnifying glass. You will then see a grid of boxes that contain articles related to your topic. Clicking the article text will open a new tab window and will show a Wiki page about the article title that you selected. After exploring, you can close the tab window and navigate back up to the main application page. At the bottom of the page you can view a random dog picture by clicking on the "Ruff" button. You may click as many times as you wish and will generate a random dog every time you do so. To view the history of what you have searched in the search bar, click on the "history" button next to the magnifying glass. You will then see a modal pop-up with a list of the items you have searched. You can click on the X on the right side of the modal to go back to the main page. To clear your history of searches click on the "clear history" button next to history. If you click on the "history" button after you click ont he "clear history", the modal will only contain the title "Search History" with nothing else in it. To visit where the application is located, you may click on the "Github" located to the left of the navigation bar. To visit the actual Wikipedia webiste, click on the "Wiki" that's located on the right side of the navigation bar.
+
+## Credits
+
+### Collaborators:
+Deandra Diedrick,
+Josh Deino,
+Kenyon Gordon,
+Paola Guerrero
+
+### Tutorials:
+https://www.youtube.com/watch?v=yqwHxAH1xrw
+
+https://www.youtube.com/watch?v=yqwHxAH1xrw
+
+### APIs
+
+https://en.wikipedia.org/api/rest_v1/#/
+
+https://dog.ceo/dog-api/
+
+
+
---
+++ assets/script.js
@@ -0,0 +1,161 @@
+const input=document.querySelector("#input");
+const searchButton=document.querySelector("#search");
+const histButton=document.querySelector("#history");
+const clearButton=document.querySelector("#clear");
+const errorSection=document.querySelector("#errors");
+const resultsSection=document.querySelector("#results");
+const history=JSON.parse(localStorage.getItem('searches')) || [];
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+var content = document.querySelector(".modal-body")
+var dateTime = new Date();
+function updateTime() {
+  const dateTime = new Date();
+  const DT = dateTime.toLocaleString();
+  document.getElementById('date-time').innerHTML = DT;
+}
+
+  // Update time every second
+setInterval(updateTime, 1000);
+
+
+
+const url = 'https://en.wikipedia.org/w/api.php'; 
+const params={
+    origin:'*',
+    format:'json',
+    action:'query',
+    prop:'extracts',
+    exchars:250,
+    exintro:true,
+    explaintext:true,
+    generator:'search',
+    gsrlimit:20,
+
+}
+const disableUi=()=>{
+    input.disabled=true;
+    searchButton.disabled=true;
+}
+const enableUi=()=>{
+    input.disabled=false;
+    searchButton.disabled=false;
+}
+const clearPrev=()=>{
+    resultsSection.innerHTML="";
+    errorSection.innerHTML="";
+
+}
+
+const isEmpty=input=>{
+    if(!input || input===''){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+const showError= error =>{
+    errorSection.innerHTML='🚨'+ error +'🚨';
+}
+
+const showResults = results => {
+    results.forEach(result => {
+        resultsSection.innerHTML += `
+        <div class="results__item">
+            <a href="https://en.wikipedia.org/?curid=${result.pageId}" target="_blank" class="card animated bounceInUp">
+                <h2 class="results__item__title">${result.title}</h2>
+                <p class="results__item__intro">${result.intro}</p>
+            </a>
+        </div>
+    `;
+    });
+};
+const gatherData = pages => {
+    const results = Object.values(pages).map(page => ({
+        pageId: page.pageid,
+        title: page.title,
+        intro: page.extract,
+    }));
+
+    showResults(results);
+};
+
+
+const getData=async ()=>{
+    const userInput=input.value;
+    history.push(userInput);
+    localStorage.setItem("searches",JSON.stringify(history) );
+   
+    if(isEmpty(userInput)) return;
+    params.gsrsearch = userInput;
+    disableUi();
+    clearPrev();
+
+    try{
+   const {data}= await axios.get(url,{params});
+   if(data.error) throw new Error(data.error.info);
+   gatherData(data.query.pages);
+    }catch(error){
+        showError(error); 
+    } finally{
+        enableUi();
+    }
+  
+}
+const handleKeyEvent=(e)=>{
+    if (e.key === 'Enter'){
+        getData();
+
+    }
+
+}
+const registerEventHandlers= () =>{
+    input.addEventListener('keydown', handleKeyEvent);
+    searchButton.addEventListener('click', getData);
+    histButton.addEventListener('click', getHistory);
+    clearButton.addEventListener('click', clearHistory)
+
+}
+const getHistory= () =>{
+    var temp= JSON.parse(localStorage.getItem("searches"));
+    content.innerHTML=temp;  
+    modal.style.display = "block";
+}
+const clearHistory= () =>{
+    localStorage.clear();
+    history.length=0;
+    var message="No searches in history";
+    if(history.length==0){
+        content.innerHTML=message;
+
+    }
+    return;
+  }
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+registerEventHandlers();
+
+
+// dog section
+
+const img = document.getElementById('dogImg');
+const dogBtn = document.getElementById("dogBtn");
+
+const randDog = function() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((data) => {
+        img.innerHTML = `<img src='${data.message}'/>`;
+    });
+};
+
+dogBtn.addEventListener("click", randDog);
